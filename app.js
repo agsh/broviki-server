022bd64808b355013407f91bcb16453636e454a5
Andrew D.Laptev <a.d.laptev@gmail.com>
2016-01-19 19:17:51 +0300

    app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Andrew D.Laptev<a.d.laptev@gmail.com> on 18.01.16.
+ */
+
+const config = require('config.json')
+	, bodyParser = require('body-parser')
+	, session = require('express-session')
+	;
+
