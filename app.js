55fe4ed122c73a4466137f826e3422afb9ec6cf5
Andrew D.Laptev <a.d.laptev@gmail.com>
2016-01-22 19:18:36 +0300

    Sessions

--- app.js
+++ app.js
@@ -5,5 +5,20 @@
 const config = require('config.json')
 	, bodyParser = require('body-parser')
 	, session = require('express-session')
+	, express = require('express')
+	, app = express()
+	, NedbStore = require('connect-nedb-session')(session) // TODO replace
 	;
+
+app.use(session({
+	secret: config.secret
+	, resave: false
+	, saveUninitialized: true
+	, cookie: {
+		path: '/'
+		, secure: true
+		, maxAge: 5 * 24 * 3600 * 1000
+	}
+	, store: new NedbStore({ filename: __dirname + '/db/sessions.db' })
+}));
 
