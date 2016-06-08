daf642b5f7e1297a9c6d5e71742e0ba6be6454e0
Andrew D.Laptev <a.d.laptev@gmail.com>
2016-06-08 17:09:49 +0300

    First swagger steps

--- app.js
+++ app.js
@@ -2,12 +2,17 @@
  * Created by Andrew D.Laptev<a.d.laptev@gmail.com> on 18.01.16.
  */
 
-const config = require('config.json')
-	, bodyParser = require('body-parser')
-	, session = require('express-session')
+'use strict';
+
+const 
+	bodyParser = require('body-parser')
+	, config = require('config.json')	
 	, express = require('express')
-	, app = express()
-	, NedbStore = require('connect-nedb-session')(session) // TODO replace
+	, session = require('express-session')
+	, NedbStore = require('connect-nedb-session')(session) // TODO replace	
+	;
+
+let app = express()
 	;
 
 app.use(session({
@@ -22,3 +27,31 @@ app.use(session({
 	, store: new NedbStore({ filename: __dirname + '/db/sessions.db' })
 }));
 
+var server = http.createServer(app);
+
+server.listen(config.port);
+server.on('error', onError);
+
+function onError(error) {
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
+
+	var bind = typeof port === 'string'
+		? 'Pipe ' + port
+		: 'Port ' + port;
+
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+		case 'EACCES':
+			console.error(bind + ' requires elevated privileges');
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			console.error(bind + ' is already in use');
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
+}
